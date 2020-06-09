038f90ffc9658ebb1ff8308f11c7e961efd4acb3
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-09 14:58:17 +0000

    Automatically backed up by Learn

--- spec/06_patient_spec.rb
+++ spec/06_patient_spec.rb
@@ -45,4 +45,18 @@ describe 'Patient' do
     end
   end
 
+  describe '#doctors' do
+    it 'has many doctors through appointments' do
+      doctor_who = Doctor.new('The Doctor')
+      doctor_what = Doctor.new('Das Doktor')
+      hevydevy = Patient.new('Devin Townsend')
+      hevydevy.new_appointment(doctor_who, 'Friday, January 32nd')
+      hevydevy.new_appointment(doctor_what, 'Saturday, January 32nd')
+
+      expect(hevydevy.doctors).to include(doctor_who)
+      expect(hevydevy.doctors).to include(doctor_what)
+    end
+  end
+end
+
   
