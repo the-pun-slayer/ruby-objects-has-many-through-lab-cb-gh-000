11bb472f8c6580f528f3497f10ca6debdc50a968
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-09 14:58:39 +0000

    Done.

--- spec/04_doctor_spec.rb
+++ spec/04_doctor_spec.rb
@@ -22,7 +22,7 @@ describe 'Doctor' do
       hevydevy = Patient.new('Devin Townsend')
       appointment = Appointment.new('Friday, January 32nd', hevydevy, doctor_who)
       appointment_2 = Appointment.new('Saturday, January 33rd', hevydevy, doctor_smith)
-      
+
       expect(doctor_who.appointments).to include(appointment)
       expect(doctor_smith.appointments).to include(appointment_2)
 
@@ -40,12 +40,12 @@ describe 'Doctor' do
   end
 
   describe '#patients' do
-    it 'has many patients, through appointments' do
-      doctor_who = Doctor.new('The Doctor')
-      hevydevy = Patient.new('Devin Townsend')
-      doctor_who.new_appointment('Friday, January 32nd', hevydevy)
+   it 'has many patients, through appointments' do
+     doctor_who = Doctor.new('The Doctor')
+     hevydevy = Patient.new('Devin Townsend')
+     doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
 
-      expect(doctor_who.patients).to include(hevydevy)
-    end
-  end
+     expect(doctor_who.patients).to include(hevydevy)
+   end
+ end
 end
